349f95f2c7aee034684fee7eebc6a29f7464d8a8
Gwen <g@10degres.net>
2022-08-30 14:44:08 +0200

    api doc

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -97,7 +97,8 @@ class UserController extends Controller
             $user->save();
         }
 
-        $response = Http::withHeaders(['Content-Type: application/x-www-form-urlencoded'])->put(env('API_URL').'/users/'.$user->id, $t_datas);
+        // $response = Http::withHeaders(['Content-Type: application/x-www-form-urlencoded'])->put(env('API_URL').'/users/'.$user->id, $t_datas);
+        $response = Http::put(env('API_URL').'/users/'.$user->id, $t_datas);
         // var_dump($response);
 
         if( $response->ok() ) {
