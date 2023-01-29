c65c9fd2d8ed906259090d242258bd5d37b1bc67
Gwen <g@10degres.net>
2023-01-29 13:26:41 +0100

    user seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,14 +17,14 @@ return new class extends Migration
             $table->id();
             $table->string('username',32)->unique();
             $table->string('fullname',64);
-            $table->string('address');
-            $table->string('phone',16);
+            $table->string('address')->nullable()->default(null);
+            $table->string('phone',16)->nullable()->default(null);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('api_token',64)->unique()->nullable()->default(null);
             $table->string('picture')->nullable()->default(null);
-            $table->string('role',16)->nullable()->default(null);
+            $table->string('role',16);
             $table->rememberToken();
             $table->timestamps();
         });
